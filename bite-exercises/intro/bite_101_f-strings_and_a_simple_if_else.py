795206aadb59ab29795713a8d9e9f0e4dc33a69a
goranaviani <goranaviani@gmail.com>
2020-07-23 09:28:55 +0200

    quickfix and small refactor

---
+++ bite-exercises/intro/bite_101_f-strings_and_a_simple_if_else.py
@@ -0,0 +1,25 @@
+"""
+The latest way to print to the screen in Python (>= 3.6) is with f-strings.
+In this Bite we'll get you to calculate whether a person is able to drive or not based on their minimum age which
+is stored in the constant MIN_DRIVING_AGE.
+Print (not return) name is allowed to drive or name is not allowed to drive (note there is no period (.) at the end),
+based on the age being equal or greater than MIN_DRIVING_AGE.
+"""
+MIN_DRIVING_AGE = 18
+
+
+
+def main():
+    test_not_allowed_to_drive()
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    if age < MIN_DRIVING_AGE:
+        print('{} is allowed to drive' .format(name))
+    else:
+        print('{} is not allowed to drive' .format(name))
+
+if __name__ == '__main__':
+    main()
