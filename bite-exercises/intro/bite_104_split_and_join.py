58c984548448ae29c918157f11200fef4dfd5c9f
goranaviani <goranaviani@gmail.com>
2020-07-26 09:13:04 +0200

    test test_split_in_columns_default_message passed

--- bite-exercises/intro/bite_104_split_and_join.py
+++ bite-exercises/intro/bite_104_split_and_join.py
@@ -4,9 +4,18 @@ Have fun with our Bites of Py.
 Keep calm and code in Python!
 Become a PyBites ninja!"""
 
+def test_split_in_columns_default_message():
+    # one line string but a nice way in Python to wrap over multiple lines
+    expected = ('Hello world!|We hope that you are learning a lot of Python.|'
+                'Have fun with our Bites of Py.|Keep calm and code in Python!'
+                '|Become a PyBites ninja!')
+
+    actual = split_in_columns()
+    assert actual == expected
 
 def main():
     result = split_in_columns(message)
+    test_split_in_columns_default_message()
 
 def split_in_columns(message=message):
     """Split the message by newline (\n) and join it together on '|'
