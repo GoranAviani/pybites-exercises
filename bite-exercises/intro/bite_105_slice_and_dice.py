98170c5479b528b788bda889acc31c25ce5df0e3
goranaviani <goranaviani@gmail.com>
2020-07-26 09:20:27 +0200

    adding tests

--- bite-exercises/intro/bite_105_slice_and_dice.py
+++ bite-exercises/intro/bite_105_slice_and_dice.py
@@ -14,9 +14,33 @@ but here is the kicker: you can use this on a list too!
 and now you know about slicing from the end as well :)
 keep enjoying our bites!
 """
+another_text = """
+Take the block of text provided and strip() off the whitespace at the ends.
+Split the whole block up by newline (\n).
+ if the first character is lowercase, split it into words and add the last word
+of that line to the results list.
+Strip the trailing dot (.) and exclamation mark (!) from the word first.
+  finally return the results list!
+"""
 
+
+def test_slice_and_dice_default_text():
+    expected = ['objects', 'y', 'too', ':)', 'bites']
+    assert slice_and_dice(text) == expected
+
+
+def test_slice_and_dice_other_text():
+    expected = ['word', 'list', 'list']
+    assert slice_and_dice(another_text) == expected
 
 def slice_and_dice(text: str = text) -> list:
     """Get a list of words from the passed in text.
        See the Bite description for step by step instructions"""
     results = []
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
