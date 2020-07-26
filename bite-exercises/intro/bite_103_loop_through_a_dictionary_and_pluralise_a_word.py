7a4169ac36b47cfe9f7c14f9e2bc8ef77422f1ad
goranaviani <goranaviani@gmail.com>
2020-07-26 09:02:32 +0200

    bite 103

--- bite-exercises/intro/bite_103_loop_through_a_dictionary_and_pluralise_a_word.py
+++ bite-exercises/intro/bite_103_loop_through_a_dictionary_and_pluralise_a_word.py
@@ -4,9 +4,16 @@
     many games they've won in the following format: sara has won n games
     To make it human readable, pluralise the word game to suit the number of games won.
 """
+games_won = dict(sara=0, bob=1, tim=5, julian=3, jim=1)
+
+
 
 def main():
-    pass
+    for name, result in games_won.items():
+        game_single_plural = 'game'
+        if result > 1:
+            game_single_plural = 'games'
+        print('{} has won {} {}'. format(name, result, game_single_plural))
 
 if __name__ == '__main__':
     main()
