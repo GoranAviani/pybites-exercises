25a1be2d118cd3a60061fddf741d81f05c51226d
goranaviani <goranaviani@gmail.com>
2020-07-27 18:14:54 +0200

    test_strip_vowels_on_other_text test passed

--- bite-exercises/intro/bite_106_strip_out_vowels_and_count_the_number_of_replacements.py
+++ bite-exercises/intro/bite_106_strip_out_vowels_and_count_the_number_of_replacements.py
@@ -65,6 +65,7 @@ def strip_vowels(text: str) -> Tuple[str, int]:
 def main():
     output, number_replacements = strip_vowels(text)
     test_strip_vowels_on_zen()
+    test_strip_vowels_on_other_text()
 
 def test_strip_vowels_on_zen():
     output, number_replacements = strip_vowels(text)
@@ -76,6 +77,23 @@ def test_strip_vowels_on_zen():
     assert 'N*m*sp*c*s *r* *n* h*nk*ng gr**t *d**' in output
     assert '*lth**gh pr*ct*c*l*ty b**ts p*r*ty.' in output
 
+
+def test_strip_vowels_on_other_text():
+    text = """Hello world!
+              We hope that you are learning a lot of Python.
+              Have fun with our Bites of Py.
+              Keep calm and code in Python!
+              Become a PyBites ninja!
+              All the way"""
+
+    output, number_replacements = strip_vowels(text)
+
+    assert number_replacements == 46
+
+    assert 'H*ll* w*rld!' in output
+    assert 'H*v* f*n w*th **r B*t*s *f Py' in output
+    assert 'B*c*m* * PyB*t*s n*nj*!' in output
+    assert '*ll th* w*y' in output
 
 
 if __name__ == '__main__':
