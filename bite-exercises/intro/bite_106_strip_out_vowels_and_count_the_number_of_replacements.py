403abf55fb079c72013d27b2c5968f8b185f3b4a
goranaviani <goranaviani@gmail.com>
2020-07-27 18:10:14 +0200

    adding vowels and text, changing fun's positions

--- bite-exercises/intro/bite_106_strip_out_vowels_and_count_the_number_of_replacements.py
+++ bite-exercises/intro/bite_106_strip_out_vowels_and_count_the_number_of_replacements.py
@@ -9,20 +9,30 @@
     Bonus: if you already have some Python under your belt, try to use re and try to solve it without a for loop :)
 """
 
-def test_strip_vowels_on_zen():
-    output, number_replacements = strip_vowels(text)
+text = """
+The Zen of Python, by Tim Peters
 
-    assert number_replacements == 262
-
-    assert 'Th* Z*n *f Pyth*n, by T*m P*t*rs' in output
-    assert 'B***t*f*l *s b*tt*r th*n *gly' in output
-    assert 'N*m*sp*c*s *r* *n* h*nk*ng gr**t *d**' in output
-    assert '*lth**gh pr*ct*c*l*ty b**ts p*r*ty.' in output
-
-
-def main():
-    pass
-
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+vowels = 'aeiou'
 
 def strip_vowels(text: str) -> Tuple[str, int]:
     """Replace all vowels in the input text string by a star
@@ -39,6 +49,23 @@ def strip_vowels(text: str) -> Tuple[str, int]:
        of Python's new type hinting:
        https://docs.python.org/3/library/typing.html"""
     pass
+
+
+
+def main():
+    pass
+
+def test_strip_vowels_on_zen():
+    output, number_replacements = strip_vowels(text)
+
+    assert number_replacements == 262
+
+    assert 'Th* Z*n *f Pyth*n, by T*m P*t*rs' in output
+    assert 'B***t*f*l *s b*tt*r th*n *gly' in output
+    assert 'N*m*sp*c*s *r* *n* h*nk*ng gr**t *d**' in output
+    assert '*lth**gh pr*ct*c*l*ty b**ts p*r*ty.' in output
+
+
 
 if __name__ == '__main__':
     main()
