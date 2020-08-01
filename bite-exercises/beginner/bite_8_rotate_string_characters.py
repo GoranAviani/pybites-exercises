dd5bede8f5c2b19c7892ecb63bf7623e12f61800
goranaviani <goranaviani@gmail.com>
2020-08-01 19:31:31 +0200

    bite 8 passing tests

--- bite-exercises/beginner/bite_8_rotate_string_characters.py
+++ bite-exercises/beginner/bite_8_rotate_string_characters.py
@@ -11,6 +11,17 @@ def test_small_rotate():
     assert rotate('hello', 2) == 'llohe'
     assert rotate('hello', -2) == 'lohel'
 
+def test_bigger_rotation_of_positive_n():
+    string = 'bob and julian love pybites!'
+    expected = 'love pybites!bob and julian '
+    assert rotate(string, 15) == expected
+
+def test_bigger_rotation_of_negative_n():
+    string = 'pybites loves julian and bob!'
+    expected = 'julian and bob!pybites loves '
+    assert rotate(string, -15) == expected
+
+
 
 def rotate(string, n):
     """Rotate characters in a string.
@@ -25,6 +36,9 @@ def main():
     result = rotate('hello', 2)
     result = rotate('hello', -2)
     test_small_rotate()
+    test_bigger_rotation_of_positive_n()
+    test_bigger_rotation_of_negative_n()
+
 
 if __name__ == '__main__':
     main()
