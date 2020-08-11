ec3e3ffe283368ebdc8633f83a574a2e13aec8f8
goranaviani <goranaviani@gmail.com>
2020-08-11 18:29:03 +0200

    wip - test test_load_words passed

--- bite-exercises/intermediate/bite_3_word_values.py
+++ bite-exercises/intermediate/bite_3_word_values.py
@@ -22,6 +22,7 @@ def test_load_words():
     assert words[0] == 'A'
     assert words[-1] == 'Zyzzogeton'
     assert ' ' not in ''.join(words)
+    print("Test passed")
 
 scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
                    (4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
