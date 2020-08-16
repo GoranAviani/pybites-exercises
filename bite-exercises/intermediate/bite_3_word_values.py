7da1574765d7a3b3aabc2715e374177742fb67f4
goranaviani <goranaviani@gmail.com>
2020-08-16 15:40:43 +0200

    test test_max_word_value passed

--- bite-exercises/intermediate/bite_3_word_values.py
+++ bite-exercises/intermediate/bite_3_word_values.py
@@ -36,6 +36,7 @@ def test_max_word_value():
     assert max_word_value(test_words) == 'barbeque'
     words = load_words()
     assert max_word_value(words[20000:21000]) == 'benzalphenylhydrazone'
+    print("Test passed..")
 
 scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
                    (4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
